e5320d6bb7c8eef634898282f67c1e9089f69d59
aishwaryasthakur <thakuraishwarya310@gmail.com>
2020-09-15 21:07:44 +0530

    Day 1 Assignment

---
+++ Assignment/src/com/training/Application.java
@@ -0,0 +1,17 @@
+package com.training;
+
+public class Application {
+	
+	public static void main(String [] args)
+	{
+		//Object Creation using parameterized constructor
+		BloodDonor bd = new BloodDonor("Aishwarya", 22, "A+");
+		System.out.println(bd.getName() + " " + bd.getAge() + " " + bd.getBloodGroup());
+		
+		//Setter method application
+		bd.setAge(23);
+		//Getter method application
+		System.out.println(bd.getName() + " " + bd.getAge() + " " + bd.getBloodGroup());
+	}
+
+}
